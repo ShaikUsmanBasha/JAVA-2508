5555d0f0ad49ffb641ca53453f3279b24da997f0
ShaikUsmanBasha <usmanbashashaik524@gmail.com>
2025-06-19 04:50:55 +0530

    variables changed

---
+++ VariablesDemo.java
@@ -0,0 +1,8 @@
+public class VariablesDemo{
+           public static void main(String[] args) {
+                        int age=20;
+                        String name= "usman";
+                        String message="welcome to java program";
+                        System.out.println(message);   
+           }
+}
